d6efd0caba03474b8149eaeb53bf11568d146d10
PonpokoFes <42681393+PonpokoFes@users.noreply.github.com>
2020-04-21 16:28:34 +0900

    初回コミット
    移植用.netFrameWorkの日付書きこみのコンソールアプリ

---
+++ Framework/PortingTestFramework/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+
+namespace PortingTestFramework
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = ConfigurationManager.AppSettings["FilePath"];
+
+            // make file 
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            // stream reader
+            var sr = new StreamReader(path);
+
+            // read file 
+            Console.WriteLine(sr.ReadToEnd());
+
+            // close stream
+            sr.Dispose();
+
+            // stream writer
+            var sw = new StreamWriter(path);
+            // append new line
+            sw.WriteLine(DateTime.Now.ToString());
+            // close stream
+            sw.Dispose();
+
+            // last check
+            var lastStream = new StreamReader(path);
+            Console.WriteLine(lastStream.ReadToEnd());
+            // close stream
+            lastStream.Dispose();
+
+            Environment.Exit(0);
+        }
+    }
+}
